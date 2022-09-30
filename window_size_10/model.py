524ae4933dc204b65f80a138fdfacfe015655320
Kexin Feng <fkx0814@gmail.com>
2022-09-30 16:46:46 -0500

    initial commit

---
+++ window_size_10/model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torchsummary import summary
+import math
+from collections import OrderedDict
+import torch.nn.init as init
+from torch.autograd import Variable
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv1d(in_channels=128, out_channels=32, kernel_size=5)
+        # self.pool1 = nn.MaxPool1d(kernel_size=2)
+        # self.conv2 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=3)
+        # self.pool2 = nn.MaxPool1d(kernel_size=2)
+        self.fc1 = nn.Linear(192, 128)
+        self.fc2 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        # x = self.pool2(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+
+# net = CNN().cuda()
+# print(summary(net, (128, 10)))
